2b159b7450117e57bc7f7e29f91c2c3a2443d59e
张界超 <12123131@qq.com>
2020-06-09 23:30:03 +0800

    springcloud-Eureka集群2

--- springcloud-consumer-dept-8080/src/main/java/com/jay/springcloud/DeptConsumer_8080.java
+++ springcloud-consumer-dept-8080/src/main/java/com/jay/springcloud/DeptConsumer_8080.java
@@ -2,6 +2,7 @@ package com.jay.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Description:
@@ -10,7 +11,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @version 1.0
  * @date 2020/6/9 21:47
  */
+//Ribbon 和 Eureka 整合以后，客户端可以直接调用，不用关心IP地址和端口号
 @SpringBootApplication
+@EnableEurekaClient //开启Eureka 客户端
 public class DeptConsumer_8080 {
     public static void main(String[] args) {
         SpringApplication.run(DeptConsumer_8080.class,args);
